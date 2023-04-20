a1f8e7c739e8e6ec2027c3bfa4af569cbd143b40
Gennaro Bellizio <g.bellizio@gmail.com>
2023-04-20 12:29:17 +0200

    Fix compilation error

--- monopoly/main.cpp
+++ monopoly/main.cpp
@@ -5,9 +5,9 @@
 #include "Monolopy.h"
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
-    using namespace std::string_view_literals;
+    using namespace std::string_literals;
 
-    const auto names = std::vector{"A"sv, "B"sv};
+    const auto names = std::vector{"A"s, "B"s};
     auto monopoly = Monopoly(names);
 
     return 0;
